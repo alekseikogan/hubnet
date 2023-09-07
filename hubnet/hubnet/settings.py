d37b55f03909437d2b3834ddda9cdd934cc0e9d0
Алексей Коган <alekseikogan@yandex.ru>
2023-09-07 22:09:01 +0300

    добавил пользовательские теги/фильтры

--- hubnet/hubnet/settings.py
+++ hubnet/hubnet/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
