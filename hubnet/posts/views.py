e87294e4744a53fb02251bc0e9af299ec2a5cda4
Алексей Коган <alekseikogan@yandex.ru>
2023-09-09 10:20:57 +0300

    добавил контекстный процессор Год и пагинацию

--- hubnet/posts/views.py
+++ hubnet/posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'title': 'Последние обновления',
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
