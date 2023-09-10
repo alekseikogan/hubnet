eaa5aed6eccf9e66f5dc137842e54a881157e746
Алексей Коган <alekseikogan@yandex.ru>
2023-09-10 18:22:03 +0300

    добавил страницу пользователя и поста

--- hubnet/posts/views.py
+++ hubnet/posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 
-from .models import Group, Post
+from .models import Group, Post, User
 
 
 def index(request):
@@ -26,3 +26,27 @@ def group_posts(request, slug):
         'group_name': group_name,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def post_detail(request, post_id):
+    current_post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'current_post': current_post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    username = user.username
+    count_posts = user.posts.count()
+    posts = user.posts.select_related('group')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'username': username,
+        'count_posts': count_posts,
+        'page_obj': page_obj,
+        }
+    return render(request, 'posts/profile.html', context)
